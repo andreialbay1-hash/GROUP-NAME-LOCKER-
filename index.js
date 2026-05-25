4f85601442570f94f1ec0262e3043e5b7b3f4ac0
andreialbay1-hash <andreialbay1@gmail.com>
2026-05-25 15:40:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 
 const appState = JSON.parse(fs.readFileSync("appstate.json", "utf-8"));
 
-const GROUP_THREAD_ID = "24041654888825173";
-const LOCKED_GROUP_NAME = "TUSHAR KE PAPA AVI HERE:)";
+const GROUP_THREAD_ID = "869377152848649";
+const LOCKED_GROUP_NAME = "Trippin lgc)";
 
 login({ appState }, (err, api) => {
   if (err) return console.error("Login Failed:", err);
@@ -28,7 +28,7 @@ login({ appState }, (err, api) => {
         console.log("✅ Group name is correct.");
       }
     });
-  }, 60000); // Check every 60 sec
+  }, 60000); // Check every 0 sec
 });
 
 // 🟢 Dummy Express server to keep Render service alive
